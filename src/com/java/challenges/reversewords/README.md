c74be5d4e27bb5108ea0bb0c48539effbdfe6079
Ankush Gupta <no.1ankush@gmail.com>
2018-08-05 12:40:33 -0500

    Added Reverse Words in String Challenge

---
+++ src/com/java/challenges/reversewords/README.md
@@ -0,0 +1,39 @@
+# Reverse Words in a String
+
+## Problem Statement: ##
+
+Given a string, reverse the string word by word. 
+
+**Assumption:** 
+* A word is defined as a sequence of non-space characters.
+* The input string does not contain leading or trailing spaces.
+* The words are always separated by a single space.
+
+## Sample Input/Output: ##
+
+<table>
+    <thead>
+        <tr>
+            <th>Scenario # </th>
+            <th>Input</th>
+            <th>Output</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>My Name is Ankush Gupta</td>
+            <td>Gupta Ankush is Name My</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>The Sky is Blue</td>
+            <td>Blue is Sky The</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Hello</td>
+            <td>Hello</td>
+        </tr>
+    </tbody>
+</table>
---
+++ src/com/java/challenges/reversewords/ReverseWordsInString.java
@@ -0,0 +1,72 @@
+package com.java.challenges.reversewords;
+
+public class ReverseWordsInString {
+
+	public static void main(String[] args) {
+		
+		char[] inputChars = "My Name is Ankush Gupta".toCharArray();
+		System.out.println("\nInput: " + new String(inputChars));
+		reverseWords(inputChars);
+		System.out.println("\nOutput: " + new String(inputChars));
+		
+		inputChars = "The Sky is Blue".toCharArray();
+		System.out.println("\nInput: " + new String(inputChars));
+		reverseWords(inputChars);
+		System.out.println("\nOutput: " + new String(inputChars));
+		
+		inputChars = "Hello".toCharArray();
+		System.out.println("\nInput: " + new String(inputChars));
+		reverseWords(inputChars);
+		System.out.println("\nOutput: " + new String(inputChars));
+	}
+	
+	/**
+	 * Returns the Reversed String
+	 * 
+	 * @param inputChars
+	 * @return String
+	 */
+	private static void reverseWords(char[] inputChars) {
+		
+		//Handle Null, Single Character and Single Word Scenarios
+		if(inputChars == null || inputChars.length <= 1 
+				|| new String(inputChars).indexOf(' ') == -1) {
+			return;
+		}
+		
+		//Reverse the Entire String
+		reverseString(inputChars, 0, inputChars.length - 1);
+		
+		//Reverse each Word in String
+		int start = 0;
+		for(int end = 0; end < inputChars.length; end ++) {
+			if(inputChars[end] == ' ') {
+				reverseString(inputChars, start, end - 1); //end - 1 to remove the space
+				start = end + 1; // end + 1 as the word starts after space
+			}
+			
+			if(end == inputChars.length - 1) {
+				reverseString(inputChars, start, end);
+			}
+		}
+	}
+	
+	/**
+	 * Reverses the String from a given start and end index
+	 * @param inputChars
+	 * @param start
+	 * @param end
+	 */
+	private static void reverseString(char[] inputChars, int start, int end) {
+		
+		if(inputChars == null || inputChars.length <= 1) {
+			return;
+		}
+		
+		for(; start < end; start ++, end --) {
+			char temp = inputChars[start];
+			inputChars[start] = inputChars[end];
+			inputChars[end] = temp;
+		}
+	}
+}
