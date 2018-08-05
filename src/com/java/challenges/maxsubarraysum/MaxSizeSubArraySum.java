2393146a6b98d6926497381847b5ac56fdb680f4
Ankush Gupta <no.1ankush@gmail.com>
2018-08-05 14:25:28 -0500

    Added Max Size sub Array Sum Challenge

---
+++ src/com/java/challenges/maxsubarraysum/MaxSizeSubArraySum.java
@@ -0,0 +1,60 @@
+package com.java.challenges.maxsubarraysum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxSizeSubArraySum {
+
+	public static void main(String[] args) {
+		
+		int[] inputNums = new int[]{1, -1, 5, -2, 3};
+		int target = 3;
+		System.out.println("\nInput: " + Arrays.toString(inputNums) + "\nTarget: " + target);
+		System.out.println("\nOutput: " + maxSubArrayLen(inputNums, target));
+		
+		inputNums = new int[]{-2, -1, 2, 1};
+		target = 1;
+		System.out.println("\nInput: " + Arrays.toString(inputNums) + "\nTarget: " + target);
+		System.out.println("\nOutput: " + maxSubArrayLen(inputNums, target));
+		
+		inputNums = new int[]{8, 11, -5, 4};
+		target = 3;
+		System.out.println("\nInput: " + Arrays.toString(inputNums) + "\nTarget: " + target);
+		System.out.println("\nOutput: " + maxSubArrayLen(inputNums, target));
+	}
+	
+	/**
+	 * Returns the maximum size sub array who's sum equals the given target.
+	 * 
+	 * @param inputChars
+	 * @return String
+	 */
+	private static int maxSubArrayLen(int[] inputNums, int target) {
+		int maxSubArraySize = 0;
+		int sum = 0;
+		
+		if(inputNums == null || inputNums.length == 0) {
+			return maxSubArraySize; 
+		}
+		
+		Map<Integer, Integer> hashMap = new HashMap<>();
+		
+		for(int i = 0; i < inputNums.length; i ++) {
+			sum += inputNums[i];
+			if(sum == target) {
+				maxSubArraySize = i + 1;
+			}
+			
+			if(hashMap.containsKey(sum - target)) {
+				maxSubArraySize = Math.max(maxSubArraySize, i - hashMap.get(sum - target));
+			}
+			
+			if(!hashMap.containsKey(sum)) {
+				hashMap.put(sum, i);
+			}
+		}
+		
+		return maxSubArraySize;
+	}
+}
---
+++ src/com/java/challenges/maxsubarraysum/README.md
@@ -0,0 +1,41 @@
+# Maximum Size Subarray Sum
+
+## Problem Statement: ##
+
+Given an array of integers and a target, return the maximum length of a subarray that sums to the specified target. If there is no matching sum then return 0
+
+## Sample Input/Output: ##
+
+<table>
+    <thead>
+        <tr>
+            <th rowspan="2">Scenario # </th>
+            <th colspan="2">Input</th>
+            <th rowspan="2">Output</th>
+        </tr>
+        <tr>
+            <th>Integers</th>
+            <th>Target</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>[1, -1, 5, -2, 3]</td>
+            <td>3</td>
+            <td>4</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>[-2, -1, 2, 1]</td>
+            <td>1</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>[8, 11, -5, 4]</td>
+            <td>3</td>
+            <td>0</td>
+        </tr>
+    </tbody>
+</table>
