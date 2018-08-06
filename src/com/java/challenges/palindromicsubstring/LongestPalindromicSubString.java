8e33377799a5b78d474f59dcae290c8bdf4b3c08
Ankush Gupta <no.1ankush@gmail.com>
2018-08-06 09:41:40 -0500

    Added Palindromic Substring Challenge

---
+++ src/com/java/challenges/palindromicsubstring/LongestPalindromicSubString.java
@@ -0,0 +1,73 @@
+package com.java.challenges.palindromicsubstring;
+
+/**
+ * @author Ankush Gupta
+ * 
+ * Email: no.1ankush@gmail.com 
+ * GitHub: https://github.com/no-1ankush
+ * LinkedIn: www.linkedin.com/in/no1ankush
+ */
+public class LongestPalindromicSubString {
+
+	public static void main(String[] args) {
+		String inputString = "babad";
+		System.out.println("\nInput: " + inputString);
+		System.out.println("\nOutput: " + longestPalindrome(inputString));
+
+		inputString = "cbbd";
+		System.out.println("\nInput: " + inputString);
+		System.out.println("\nOutput: " + longestPalindrome(inputString));
+		
+		inputString = "xytattarrattatyz";
+		System.out.println("\nInput: " + inputString);
+		System.out.println("\nOutput: " + longestPalindrome(inputString));
+	}
+
+	/**
+	 * Returns the longest Palindromic Sub string in a given String
+	 * 
+	 * @param inputString
+	 * @return String
+	 */
+	public static String longestPalindrome(String inputString) {
+		String longestPalindrome = "";
+		int maxPalindromeLength = 0;
+		
+		for(int i=0; i < inputString.length(); i++) {
+			if(isPalindrome(inputString, i-maxPalindromeLength, i)) {
+				longestPalindrome = inputString.substring(i-maxPalindromeLength, i + 1);
+				maxPalindromeLength += 1; 
+			}
+			else if(isPalindrome(inputString, i-maxPalindromeLength-1, i)) {
+				longestPalindrome = inputString.substring(i-maxPalindromeLength-1, i + 1);
+				maxPalindromeLength += 2;
+			}
+		}
+		
+		return longestPalindrome;
+	}
+	
+	/**
+	 * Checks if the given string is a Palindrome from the given Start Index and End Index
+	 * 
+	 * @param inputString
+	 * @param startIndex
+	 * @param endIndex
+	 * @return boolean
+	 */
+	private static boolean isPalindrome(String inputString, int startIndex, int endIndex) {
+		if(startIndex < 0) {
+			return false;
+		}
+		
+		while(startIndex < endIndex) {
+			if(inputString.charAt(startIndex) != inputString.charAt(endIndex)) {
+				return false;
+			}
+			startIndex++;
+			endIndex--;
+		}
+		
+		return true;
+	}
+}
