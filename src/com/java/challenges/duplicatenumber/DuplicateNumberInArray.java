5e9bc0a654bc8a54767dffb59bfecb0535e2331f
Ankush Gupta <no.1ankush@gmail.com>
2018-08-06 16:26:32 -0500

    Add Duplicate Number Challenge

---
+++ src/com/java/challenges/duplicatenumber/DuplicateNumberInArray.java
@@ -0,0 +1,53 @@
+package com.java.challenges.duplicatenumber;
+
+import java.util.Arrays;
+
+public class DuplicateNumberInArray {
+
+	public static void main(String[] args) {
+		
+		int[] inputInts = new int[]{1,3,4,2,2};
+		System.out.println("\nInput: " + Arrays.toString(inputInts));
+		System.out.println("\nOutput: " + duplciateNumber(inputInts));
+		
+		inputInts = new int[] {3,2,5,14,5,5,19,18,11,10,1,4,5,5,5,5,12,5,17,5};
+		System.out.println("\nInput: " + Arrays.toString(inputInts));
+		System.out.println("\nOutput: " + duplciateNumber(inputInts));
+		
+		inputInts = new int[] {3,2,5,14,5,5,19,18,11,10,1,4,5,5,5,5,12,5,17,5};
+		System.out.println("\nInput: " + Arrays.toString(inputInts));
+		System.out.println("\nOutput: " + duplciateNumber(inputInts));	}
+	
+	/**
+	 * Returns the index of the First Unique Character in a given String
+	 * 
+	 * @param inputInts
+	 * @return int
+	 */
+	private static int duplciateNumber(int[] inputInts) {
+		int firstDuplicateInt = -1;
+		
+        if (inputInts == null || inputInts.length < 2) {
+        	return firstDuplicateInt;
+        }
+        
+        int slow = inputInts[0];
+        int fast = inputInts[0];
+        
+        while (true) {
+            slow = inputInts[slow];
+            fast = inputInts[inputInts[fast]];
+            
+            if (slow == fast) {
+                fast = inputInts[0];
+                while (slow != fast) {
+                    slow = inputInts[slow];
+                    fast = inputInts[fast];
+                }
+                firstDuplicateInt = slow;
+                break;
+            }
+        }
+		return firstDuplicateInt;
+	}
+}
---
+++ src/com/java/challenges/duplicatenumber/README.md
@@ -0,0 +1,36 @@
+# Duplicate Number in Array
+
+## Problem Statement: ##
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+**Note:**
+
+* You must not modify the array (assume the array is read only).
+* You must use only constant, O(1) extra space.
+* Your runtime complexity should be less than O(n2).
+* There is only one duplicate number in the array, but it could be repeated more than once.
+
+## Sample Input/Output: ##
+
+<table>
+    <thead>
+        <tr>
+            <th>Scenario # </th>
+            <th>Input</th>
+            <th>Output</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>[1, 3, 4, 2, 2]</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>[3, 2, 5, 14, 5, 5, 19, 18, 11, 10, 1, 4, 5, 5, 5, 5, 12, 5, 17, 5]</td>
+            <td>5</td>
+        </tr>
+    </tbody>
+</table>
