14ba8ed3357276f85264c704de6b98a9d72dd264
Ankush Gupta <no.1ankush@gmail.com>
2018-08-06 20:50:46 -0500

    Added Add Two LinkedList Challenge

---
+++ src/com/java/challenges/linkedlist/addnumbers/AddTwoNumbersInLinkedList.java
@@ -0,0 +1,92 @@
+package com.java.challenges.linkedlist.addnumbers;
+
+public class AddTwoNumbersInLinkedList {
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+		
+		ListNode(int x) { 
+			val = x; 
+		}
+	}
+	
+	public static void main(String[] args) {
+		ListNode l11 = new ListNode(2);
+		ListNode l12 = new ListNode(4);
+		l11.next = l12;
+		ListNode l13 = new ListNode(3);
+		l12.next = l13;
+		
+		ListNode l21 = new ListNode(5);
+		ListNode l22 = new ListNode(6);
+		l21.next = l12;
+		ListNode l23 = new ListNode(4);
+		l22.next = l23;
+		
+		System.out.println("Input List Node 1: " + printListNode(l11));
+		System.out.println("Input List Node 2: " + printListNode(l21));
+		System.out.println("\nOutput List Node: " + printListNode(addTwoNumbers(l11, l21)));
+	}
+	
+	/**
+	 * Converts the List Node into a String
+	 * 
+	 * @param inputNode
+	 * @return String
+	 */
+	private static String printListNode(ListNode inputNode) {
+		StringBuilder sb = new StringBuilder();
+		while(inputNode != null) {
+			sb.append(inputNode.val + ",");
+			inputNode = inputNode.next;
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * Returns Head ListNode for the sum of two input LinkedList
+	 * 
+	 * @param l1
+	 * @param l2
+	 * @return ListNode
+	 */
+	private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode resultHeadNode = null;
+		ListNode previousNode = null;
+		
+		int sum = 0;
+		int carry = 0;
+		
+		while(l1 != null || l2 != null || carry != 0) {
+			
+			ListNode currentNode = new ListNode(0);
+			
+			int v1 = l1 != null ? l1.val : 0;
+			int v2 = l2 != null ? l2.val : 0;
+			
+			//Sum of values of two nodes
+			sum = v1 + v2 + carry;
+			
+			//Carry over of two nodes
+			carry = sum / 10;
+			
+			//Value of the Current Node
+			currentNode.val = sum % 10;
+			
+			if(resultHeadNode == null) {
+				resultHeadNode = currentNode;
+				previousNode = resultHeadNode;
+			}
+			else {
+				previousNode.next = currentNode;
+				previousNode = currentNode;
+			}
+			
+			l1 = l1 != null ? l1.next: null;
+			l2 = l2 != null ? l2.next: null;
+		}
+		
+		return resultHeadNode;
+	}
+}
---
+++ src/com/java/challenges/linkedlist/addnumbers/README.md
@@ -0,0 +1,45 @@
+# Two Sum in an Array
+
+## Problem Statement: ##
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+**Assumption:** 
+* Each input would have exactly one solution.
+* Same element can not be used twice.
+
+## Sample Input/Output: ##
+
+<table>
+    <thead>
+        <tr>
+            <th rowspan="2">Scenario # </th>
+            <th colspan="2">Input</th>
+            <th rowspan="2">Output</th>
+        </tr>
+        <tr>
+            <th>Integers</th>
+            <th>Target</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>[2, 7, 11, 15]</td>
+            <td>9</td>
+            <td>[0, 1]</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>[2, 3, 1, 5]</td>
+            <td>3</td>
+            <td>[0, 2]</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>[8, 11, -5, 4]</td>
+            <td>6</td>
+            <td>[1, 2]</td>
+        </tr>
+    </tbody>
+</table>
